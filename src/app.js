c01ebd259746c620c93739c44127f28c55b173b2
cristhian229 <cristhianvasquez99@gmail.com>
2024-07-19 18:29:45 -0500

    creacion de proyecto node anime

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import errorHandler from "./middleware/error.handler.js";
+import routerAnime from "./routes/animes.js";
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 3010;
+
+app.use(express.json());
+app.use("/animes", routerAnime);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+    console.log(
+        `El puerto esta siendo escuchado correctamente en http://localhost:${PORT}`
+    );
+});
